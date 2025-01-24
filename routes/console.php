7ae602698e00e2a3ff75283ef5488fa4f9aa22e4
Thibaud Dauce <git@hadibut.fr>
2025-01-24 14:13:40 +0100

    Add schedule

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::call(function () {
+    (new Lektrico('192.168.68.101'))->start();
+})->dailyAt('22:15');
+Schedule::call(function () {
+    (new Lektrico('192.168.68.101'))->stop();
+})->dailyAt('05:55');
